ee0e488111720a6065b977ad448ec4d4ce84733a
RegisteDjessou <41454186+RegisteDjessou@users.noreply.github.com>
2020-09-12 08:40:48 +0100

    Add files via upload

---
+++ exercice3_atelier1.py
@@ -0,0 +1,30 @@
+"""
+#Auteur: Watt_Registe
+#Date: 07/09/20
+#Version: 1
+#Description: Verifie si quelqu'un est imposable ou non
+"""
+
+def impots(age: int, sexe: str):
+    """ Verifie si quelqu'un est imposable ou non """
+    SEXE_MASCULIN = "M"
+    SEXE_FEMININ = "F"
+    MIN_IMPOSABLE_H = 20
+    MIN_IMPOSABLE_F = 18
+    MAX_IMPOSABLE_F = 35
+    
+    if(sexe == SEXE_MASCULIN or sexe == SEXE_FEMININ):
+        if( (sexe == SEXE_MASCULIN and age >MIN_IMPOSABLE_H) or (sexe == SEXE_FEMININ and age >=MIN_IMPOSABLE_F and age <=MAX_IMPOSABLE_F )):
+            resultat = "Paie les impots"
+        else:
+            resultat = "Ne paie pas les impots"
+    else:
+        resultat= "Votre sexe est incorrect"
+
+    return resultat
+
+print(impots(16, "F"))
+print(impots(30, "M"))
+
+
+
