b2d56dadeaba5067b8fba40f46ae7ffed1fbcc39
Ghinevra <71076162+Ghinc@users.noreply.github.com>
2020-09-13 00:29:27 +0200

    Update exercice6_atelier1.py

--- exercice6_atelier1.py
+++ exercice6_atelier1.py
@@ -22,7 +22,7 @@ def frais_mensuel(nbre_km: float, type_carburant: str, cylindree: int, prix_carb
             if(cylindree > CYLINDRE_2000): 
                 frais_mensuel=(NBRE_LITRE_PAR_KM_1*(nbre_km/12)*prix_carburant)*SURCOUT_1
             elif (cylindree < CYLINDRE_2000): #conditon inutile: si cylindre n'est pas plus petit que 2000,il sera forcément plus grand...
-                #avec ces contions, tu risques d'avoir des erreurs si le cylindre est égal à 2000 (car tu traites seulement strictement inférieur et strictement supérieur...
+                #avec ces contdiions, tu risques d'avoir des erreurs si le cylindre est égal à 2000 (car tu traites seulement strictement inférieur et strictement supérieur...)
                 frais_mensuel=(NBRE_LITRE_PAR_KM_2*(nbre_km/12)*prix_carburant)*SURCOUT_1
         else:
             frais_mensuel = NBRE_LITRE_PAR_KM_2*(nbre_km/12)*prix_carburant*SURCOUT_2
