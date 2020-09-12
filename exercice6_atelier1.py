9eb32d665dc0c1893a55442a43a264468e68e371
Ghinevra <71076162+Ghinc@users.noreply.github.com>
2020-09-13 00:29:01 +0200

    Update exercice6_atelier1.py

--- exercice6_atelier1.py
+++ exercice6_atelier1.py
@@ -13,15 +13,16 @@ def frais_mensuel(nbre_km: float, type_carburant: str, cylindree: int, prix_carb
     NBRE_LITRE_PAR_KM_1 = 0.1
     NBRE_LITRE_PAR_KM_2 = 0.08
     SURCOUT_1 = 1.5
-    SURCOUT_2 = 1.7
+    SURCOUT_2 = 1.7 #un commentaire pour expliquer à quoi correspondent les surcoûts pourrait être utile
     
     if(type_carburant == TYPE_CARBURANT_ESSENCE or type_carburant == TYPE_CARBURANT_DIESEL):
         
     
         if (type_carburant == TYPE_CARBURANT_ESSENCE):
-            if(cylindree > CYLINDRE_2000):
+            if(cylindree > CYLINDRE_2000): 
                 frais_mensuel=(NBRE_LITRE_PAR_KM_1*(nbre_km/12)*prix_carburant)*SURCOUT_1
-            elif (cylindree < CYLINDRE_2000):
+            elif (cylindree < CYLINDRE_2000): #conditon inutile: si cylindre n'est pas plus petit que 2000,il sera forcément plus grand...
+                #avec ces contions, tu risques d'avoir des erreurs si le cylindre est égal à 2000 (car tu traites seulement strictement inférieur et strictement supérieur...
                 frais_mensuel=(NBRE_LITRE_PAR_KM_2*(nbre_km/12)*prix_carburant)*SURCOUT_1
         else:
             frais_mensuel = NBRE_LITRE_PAR_KM_2*(nbre_km/12)*prix_carburant*SURCOUT_2
@@ -30,6 +31,6 @@ def frais_mensuel(nbre_km: float, type_carburant: str, cylindree: int, prix_carb
         
     return frais_mensuel
 
-print(frais_mensuel(1200, "E", 2100, 1))
+print(frais_mensuel(1200, "E", 2100, 1)) 
 
 #15
